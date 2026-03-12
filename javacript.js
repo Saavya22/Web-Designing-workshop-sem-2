f0597602f50dcd2884e07af72f1e934cd57dd779
Saavya22 <saavya56@gmail.com>
2026-03-12 16:09:27 +0530

    Add files via upload

---
+++ javacript.js
@@ -0,0 +1,23 @@
+console.log("new file")
+  for (let i = 1; i <= 5; i++) {
+
+  
+    let row = "";
+  
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
+let name ="Sanchit";
+let age="20";
+let batch="Cse-21";
+let college="ABES Engineering College";
+let city="Ghaziabad";
+
+console.log("Name : "+name);
+console.log("Age : "+age);
+console.log("Batch : "+batch);
+console.log("College : "+college);
+console.log("City : "+city);
---
+++ n.js
@@ -0,0 +1,11 @@
+console.log("name")
+console.log("new javascript")
+for(let i=1;i<=5;i++){
+    let row="";
+    for(let j=0;j<=i;j++)
+    {
+        row+="*";
+
+    }
+    console.log(row);
+}
